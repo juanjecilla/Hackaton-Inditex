da07d4eeb92a6891eb07b9f4d3e21e0b2bfb6c65
luciatahan <luciatahan@gmail.com>
2017-10-05 18:28:12 +0200

    ManageActivity

---
+++ app/src/main/java/com/monoloco/zaraqueue/ManageActivityFragment.java
@@ -0,0 +1,22 @@
+package com.monoloco.zaraqueue;
+
+import android.support.v4.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * A placeholder fragment containing a simple view.
+ */
+public class ManageActivityFragment extends Fragment {
+
+    public ManageActivityFragment() {
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_manage, container, false);
+    }
+}
